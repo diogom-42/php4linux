e87938360fd3eca0f13780a0fb05e2c3f99f6ca1
Diogo Moura <diogomoura167@gmail.com>
2019-02-23 14:27:36 -0300

    Estrutura Base

---
+++ HandsOn/Aula02/Aula02_intro_OO_ex1.php
@@ -0,0 +1,26 @@
+<?php
+// Alteraçâo p/ testar git diff
+// Criando uma classe no PHP
+// P/ definição do nome usamos o padrão StudlyCaps
+// Sempre inciando com letra maiuscula
+class Conta
+{
+    // Atributos ou propiedades(visibilidade do nome)
+    // Para definição do nome usamos o padrão camelCase
+    public $saldo;
+
+    public $titular;
+
+    // Metodos (visibilidade nome)
+    // Metodos podem ou não receber parâmentros, e podem ou não retornar alguma coisa
+    // P/ definição do nome usamo o padrão camelCase
+    public function sacar()
+    {
+        # code...
+    }
+
+    public function depositar()
+    {
+        # code...
+    }
+}
---
+++ HandsOn/Aula02/Aula02_intro_OO_ex2.php
@@ -0,0 +1,73 @@
+<?php
+
+class Conta
+{
+    public $saldo = 0;
+    public $titular;
+
+    /**
+     * Metodo p/ fazer um saque
+     * @param float $valor
+     */
+    public function sacar($valor)
+    {
+        # code...
+        echo "<hr> Saque Efetuado Valor: <b> $valor </b><hr>";
+        $this->saldo -= $valor;
+    }
+
+    /**
+     * Metodo para fazer depósito
+     * @param float $valor
+     */
+    public function depositar($valor)
+    {
+        # code...
+        $this->saldo += $valor;
+    }
+
+    /**
+     * Retorna o saldo atual
+     * @return float $saldo
+     */
+    public function verSaldo()
+    {
+        # code...
+        return $this->saldo;
+    
+    }
+}
+
+$conta1 = new Conta();
+$conta2 = new Conta();
+
+$conta1->titular = "Luciana de Sousa Lima";
+$conta2->titular = "Aline de Oliveira";
+
+$conta1->depositar(500);
+$conta2->depositar(1000);
+
+echo '<pre>';
+var_dump($conta1);
+echo '<hr>';
+var_dump($conta2);
+
+echo '<hr>';
+echo 'Saldo Atual em Conta1: ' . $conta1->verSaldo(); 
+echo '<br>';
+echo 'Saldo Atual em Conta2: ' . $conta2->verSaldo();
+
+echo '<hr>';
+$conta1->sacar(400);
+$conta2->sacar(600);
+
+echo '<hr>';
+echo 'Saldo Atual em Conta1: ' . $conta1->verSaldo(); 
+echo '<br>';
+echo 'Saldo Atual em Conta2: ' . $conta2->verSaldo();
+
+echo '<hr>';
+echo 'Titular da Conta1: ' . $conta1->verSaldo(); 
+echo '<br>';
+echo 'Titular da Conta2: ' . $conta2->verSaldo();
+
